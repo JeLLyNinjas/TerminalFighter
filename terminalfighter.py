632ecfdc25cb7392120cc6be16972133690c5acc
Enoch Tsang <etsang1@hotmail.com>
2015-09-17 14:33:01 -0600

    game loop uses delta time to
    run game update length

--- terminalfighter.py
+++ terminalfighter.py
@@ -11,7 +11,17 @@ WHITE = 0, 0, 0
 
 gamemaster = GameMaster() 
 
+# pygame ticks, one tick is 1/1000 second
+# 15 pygame ticks per update is approximately 50 updates per second
+UPDATE_LENGTH_MS = 20    
+
+prev_update_start_time = 0
+
 while 1:
+    update_start_time = pygame.time.get_ticks()
+    print("Elapsed time since last update : " + str(update_start_time - prev_update_start_time))
+    prev_update_start_time = update_start_time
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -24,5 +34,11 @@ while 1:
     gamemaster.update()
     gamemaster.draw(screen)
 
+
     pygame.display.flip()
+
+    update_end_time = pygame.time.get_ticks()
+    update_time_elapsed = update_end_time - update_start_time 
+    if update_time_elapsed < UPDATE_LENGTH_MS:
+        pygame.time.wait(UPDATE_LENGTH_MS - update_time_elapsed)
 

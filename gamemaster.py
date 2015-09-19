e202a987f39429284a30dca8da9884473117381c
Enoch Tsang <etsang1@hotmail.com>
2015-09-18 23:48:26 -0600

    basic grunts are continuously created by gamemaster now

--- gamemaster.py
+++ gamemaster.py
@@ -12,19 +12,27 @@ class GameMaster():
 	def __init__(self, gameboard):
 		self.gameobject_id_assigner_ = IdentificationAssigner()
 		self.gameboard_ = gameboard
-		self.maincharacter_ = self.instantiateMainCharacter()
-		self.enemies_ = [self.instantiateBasicGrunt()]
+		self.maincharacter_ = self.instantiate_main_character()
+		self.enemies_ = [self.instantiate_basic_grunt()]
+		self.basic_grunt_spawn_timer = 0
+		self.basic_grunt_spawn_delay = 200
 
-	def instantiateMainCharacter(self):
+	def instantiate_main_character(self):
 		starting_position = [self.gameboard_.width_*0.5, self.gameboard_.height_*0.9]
 		return MainCharacter(starting_position, self.gameobject_id_assigner_.get_id())
 
-	def instantiateBasicGrunt(self):
+	def instantiate_basic_grunt(self):
 		starting_position = [self.gameboard_.width_*random.randint(200,800)/1000, 0]
 		return BasicGrunt(starting_position, self.gameobject_id_assigner_.get_id())
 
 	def update(self, events):
-		self.maincharacter_.update(events, self.enemies_)
+		self.maincharacter_.update(events, list(self.enemies_))
+		
+		self.basic_grunt_spawn_timer += 1
+		if self.basic_grunt_spawn_timer > self.basic_grunt_spawn_delay:
+			self.enemies_.append(self.instantiate_basic_grunt())
+			self.basic_grunt_spawn_timer = 0
+
 		for enemy in list(self.enemies_):
 			if pygame.Rect((0,0), self.gameboard_.bounds_).collidepoint(enemy.position_) :
 				enemy.update()
@@ -32,8 +40,8 @@ class GameMaster():
 				self.enemies_.remove(enemy)
 
 	def draw(self, screen):
-		self.maincharacter_.draw(screen)
 		for enemy in self.enemies_:
 			enemy.draw(screen)
 
+		self.maincharacter_.draw(screen)
 		self.maincharacter_.draw_ui(screen)

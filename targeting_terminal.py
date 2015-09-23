c3d8b5b2cf32f9075ff0a14ab376523f01a11b43
Gordon Cheung <gordoncheung2@gmail.com>
2015-09-22 22:33:18 -0600

    Optimized Class and got rid of magic (not imaginary) numbers

--- targeting_terminal.py
+++ targeting_terminal.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.font.init()
 
+# CONSTANTS
 BLACK = 0, 0, 0
 BLUE = 0, 0, 255
 WHITE = 255, 255, 255
@@ -9,30 +10,43 @@ EVENT_KEY_a = 97
 EVENT_KEY_z = 122
 EVENT_KEY_ENTER = 13
 EVENT_KEY_BACKSPACE = 8
+fontSize = 15
+rectX = 10
+rectY = 40
+rectWidth = 400
+rectHeight = 25
+maxTextWidth = 380
+borderRectWidth = 2
+textAntialias = 1
+textX = 15
+textY = 35 
 
 class TargetingTerminal():
-      
+    
     def __init__(self, DRAWING_SCALE):
         self.current_text_ = ""
-        self.ui_font_ = pygame.font.SysFont("monospace", 10*DRAWING_SCALE)
+        self.ui_font_ = pygame.font.SysFont("monospace", fontSize*DRAWING_SCALE)
         self.DRAWING_SCALE_ = DRAWING_SCALE
+        self.terminal_rect_ = None
 
     def update(self , events):
         for event in events:
-            textWidth = self.ui_font_.size(self.current_text_)[0]
             if event.type == pygame.KEYDOWN:
+                textWidth = self.ui_font_.size(self.current_text_)[0]
                 if event.key == EVENT_KEY_ENTER:
                     self.current_text_ = ""
                 if event.key == EVENT_KEY_BACKSPACE:
                     self.current_text_ = self.current_text_[:-1]
-                elif event.key in range(EVENT_KEY_a, EVENT_KEY_z) and textWidth < 380:
+                elif event.key in range(EVENT_KEY_a, EVENT_KEY_z) and textWidth < maxTextWidth:
                     self.current_text_ += event.unicode.lower()
 
     def draw_terminal(self, screen):
-        terminal_rect = pygame.Rect(10*self.DRAWING_SCALE_, screen.get_height()-(40*self.DRAWING_SCALE_), 400* self.DRAWING_SCALE_, 25*self.DRAWING_SCALE_)
-
-        pygame.draw.rect(screen, BLUE, terminal_rect)
-        pygame.draw.rect(screen, WHITE, terminal_rect, 2*self.DRAWING_SCALE_)
+        if not self.terminal_rect_:
+            self.terminal_rect_ = pygame.Rect(rectX*self.DRAWING_SCALE_, screen.get_height()-rectY*self.DRAWING_SCALE_, 
+                                              rectWidth*self.DRAWING_SCALE_, rectHeight*self.DRAWING_SCALE_)
+        
+        pygame.draw.rect(screen, BLUE,self.terminal_rect_)
+        pygame.draw.rect(screen, WHITE, self.terminal_rect_, borderRectWidth*self.DRAWING_SCALE_)
     
-        text_label = self.ui_font_.render(self.current_text_, 1, WHITE)
-        screen.blit(text_label, (15 * self.DRAWING_SCALE_, screen.get_height()-(30 * self.DRAWING_SCALE_)))
+        text_label = self.ui_font_.render(self.current_text_, textAntialias, WHITE)
+        screen.blit(text_label, (textX * self.DRAWING_SCALE_, screen.get_height()-(textY * self.DRAWING_SCALE_)))

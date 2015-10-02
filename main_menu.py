94c91b22aa648a8cad5994ef8c336ca0e78ab615
Enoch Tsang <etsang1@hotmail.com>
2015-10-02 11:47:28 -0600

    selection units are spawned

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from basic_grunt import BasicGrunt
+from destroy_listener import DestroyListener 
 from maincharacter import MainCharacter
 from universe import Universe
 
@@ -12,12 +14,48 @@ GAME_HEIGHT = 700
 
 BLACK = 0, 0, 0
 
-def main_menu(screen, DRAWING_SCALE):
-    
-    universe = Universe((GAME_WIDTH, GAME_HEIGHT))
+class SelectionListener(DestroyListener):
+
+    def __init__(self):
+        self.selections_ = dict()
+        self.selected_ = None
+
+    def register_selection(self, name, gameobject):
+        self.selections_[gameobject.ID_] = name
+        gameobject.register(self)
+
+    """
+    Listeners
+    """
+
+    def reported_destroyed(self, type_gameobject):
+        if not self.selected_:
+            self.selected_ = self.selections_[type_gameobject.ID_]
+
+def spawn_main_character(universe, DRAWING_SCALE):
     the_main_character = MainCharacter([universe.width_/2, universe.height_*0.9], universe, DRAWING_SCALE)
-    the_main_character.weapons_ = the_main_character.weapons_[:1] 
+    the_main_character.weapons_ = the_main_character.weapons_[:1]
     universe.create_main_character(the_main_character)
+
+def spawn_selection(starting_pos, name, universe, selection_listener):
+    the_selection = BasicGrunt(starting_pos, universe)
+    the_selection.speed_ = 0
+    universe.create_enemy(the_selection)
+    selection_listener.register_selection(name, the_selection)
+
+def main_menu(screen, DRAWING_SCALE):
+    universe = Universe((GAME_WIDTH, GAME_HEIGHT))
+    selection_listener = SelectionListener()
+
+    spawn_main_character(universe, DRAWING_SCALE)   
+    spawn_selection((GAME_WIDTH*(1/3), GAME_HEIGHT*(1/4)),
+                    "PLAY", 
+                    universe, 
+                    selection_listener)
+    spawn_selection((GAME_WIDTH*(2/3), GAME_HEIGHT*(1/4)),
+                    "QUIT", 
+                    universe, 
+                    selection_listener)
 
     # pygame ticks, one tick is 1/1000 second
     # 15 pygame ticks per update is approximately 30 updates per second
@@ -35,12 +73,15 @@ def main_menu(screen, DRAWING_SCALE):
         for event in events:
             if event.type == pygame.QUIT:
                 sys.exit()
+        if selection_listener.selected_:
+            print('returning ' + selection_listener.selected_)
+            return selection_listener.selected_
+
         universe.update(events)
         # print("update time : " + str(pygame.time.get_ticks() - update_start_time))
     
         draw_start_time = pygame.time.get_ticks()
         universe.main_character_.current_weapon_.draw(screen)
-        # universe.main_character_.draw_ui(screen)
         # print("draw time : " + str(pygame.time.get_ticks() - draw_start_time))
     
         flip_start_time = pygame.time.get_ticks()

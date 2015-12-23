a0c2b6f0506f58fca4db0663196f99711a7e987c
Enoch Tsang <etsang1@hotmail.com>
2015-12-23 00:40:46 -0700

    highscore updates

--- highscore_service.py
+++ highscore_service.py
@@ -5,11 +5,17 @@ def get_highscore():
         with open(HIGH_SCORE_FILE, 'r') as f:
             high_score = f.readline().rstrip('\n')
             if high_score:
-                return high_score
+                return int(high_score)
 
     except FileNotFoundError:
         return None
 
-def update_highscore(new_highscore):
+def overwrite_highscore(new_highscore):
     with open(HIGH_SCORE_FILE, "w") as f:
         f.write(str(new_highscore))
+
+def update_highscore(new_highscore):
+    if not get_highscore() or new_highscore > get_highscore():
+        overwrite_highscore(new_highscore)
+
+

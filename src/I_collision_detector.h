156bf11fded17b282cdc10b9fa9b8510e2d125a7
Enoch Tsang <etsang1@hotmail.com>
2015-12-26 22:31:42 -0700

    removed cpp files

--- src/I_collision_detector.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class I_CollisionDetector {
-public:
-	virtual void update_collisions() = 0;
-};
--- src/collision_detector.cpp
+++
@@ -1,5 +0,0 @@
-#include "collision_detector.h"
-
-void CollisionDetector::update_collisions() {
-	
-}

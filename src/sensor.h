38803f8f8c5d623646d434296cfe9012e2ae51d5
Enoch Tsang <etsang1@hotmail.com>
2015-12-29 15:38:39 -0700

    added sensor and ships

---
+++ src/sensor.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "game_object.h"
+
+class Sensor : public GameObject {
+	Sensor(double x_pos, double y_pos)
+	: GameObject(x_pos, y_pos)
+	{ }
+
+	virtual void update() = 0;
+	virtual void draw(GraphicsHandler* graphics) = 0;
+}

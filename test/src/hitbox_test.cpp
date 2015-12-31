367098b861c59a15720abc1513918d4c004c9744
Enoch Tsang <etsang1@hotmail.com>
2015-12-31 16:29:26 -0700

    removed failing test

--- test/src/hitbox_test.cpp
+++ test/src/hitbox_test.cpp
@@ -19,11 +19,3 @@ TEST (HitboxTest, simple_test){
     bool actual = Hitbox::is_overlapping(*hitbox1, *hitbox2);
     EXPECT_EQ(expected, actual);   
 }
-
-TEST (HitboxTest, bad_test){
-    Hitbox* hitbox1 = new Hitbox(0, 0, 100, 100);
-    Hitbox* hitbox2 = new Hitbox(50, 50, 100, 100);
-    bool expected = false;
-    bool actual = Hitbox::is_overlapping(*hitbox1, *hitbox2);
-    EXPECT_EQ(expected, actual);   
-}

2ba30e5c785671f9b81756b43acb672dc5d42e87
Enoch Tsang <etsang1@hotmail.com>
2015-12-31 01:19:50 -0700

    created sample test, adjusted code so all would compile, created script at beginning to compile source and tests and run tests, cleaned up makefiles, too many commits in one commit...

--- src/hitbox.h
+++ src/hitbox.h
@@ -5,7 +5,7 @@ class Hitbox {
 public: 
 	Hitbox(double x_pos, double y_pos, double width, double height); 	
 	Hitbox(SDL_Rect hitbox); 
-	bool is_overlapping(Hitbox hitbox1, Hitbox hitbox2);
+	static bool is_overlapping(Hitbox hitbox1, Hitbox hitbox2);
 
 protected:
 	SDL_Rect hitbox_; 

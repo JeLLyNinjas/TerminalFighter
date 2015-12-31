5d7df059d161b2f8998ced9ca9a63ba8e1652338
Enoch Tsang <etsang1@hotmail.com>
2015-12-31 13:47:11 -0700

    pushed installer

---
+++ installer/installer.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -e
+
+DIR=`pwd`
+
+function finish {
+	echo "Cleaning up..."
+	rm $DIR/SDL2-2.0.3.tar.gz || true
+	rm $DIR/SDL2_ttf-2.0.12.tar.gz || true
+	rm $DIR/SDL2_image-2.0.0.tar.gz || true
+	rm $DIR/SDL2_mixer-2.0.0.tar.gz || true
+	rm -rf $DIR/SDL2_image-2.0.0  || true
+	rm -rf $DIR/SDL2_ttf-2.0.12  || true
+	rm -rf $DIR/SDL2-2.0.3 || true
+	rm -rf $DIR/SDL2_mixer-2.0.0 || true
+}
+
+trap finish EXIT
+
+apt-get update --yes
+apt-get install libao-dev --yes
+apt-get install g++ --yes
+apt-get install libasound2-dev --yes
+apt-get install libpulse-dev --yes
+
+apt-get install libdbus-1-dev --yes
+apt-get install libudev-dev --yes
+
+#SDL2.0
+wget https://www.libsdl.org/release/SDL2-2.0.3.tar.gz
+tar -xzvf SDL2-2.0.3.tar.gz
+cd SDL2-2.0.3
+./configure 
+
+
+make -j4
+make install -j4
+cd ..
+
+#SDL2_ttf
+wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
+tar -xzvf SDL2_ttf-2.0.12.tar.gz
+cd SDL2_ttf-2.0.12
+./configure
+make -j4
+make install -j4
+cd ..
+
+#SDL2_image
+wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz
+tar -xzvf SDL2_image-2.0.0.tar.gz
+cd SDL2_image-2.0.0
+./configure
+make -j4
+make install -j4
+cd ..
+#SDL2_mixer
+wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz
+tar -xzvf SDL2_mixer-2.0.0.tar.gz
+cd SDL2_mixer-2.0.0
+#SMPEG
+cd external/smpeg2-2.0.0
+./configure
+make -j4
+make install -j4
+cd ../..
+./configure
+make -j4
+make install -j4
+cd ..

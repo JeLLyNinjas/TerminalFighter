c39ff28ceab1b4ea3755b3565cfaa75c1d33da0e
Byron Seto <byronseto@hotmail.com>
2016-01-04 18:55:49 -0700

    Removed tracking on libgtest.a. Added a test for hitbox_test

--- test/src/hitbox_test.cpp
+++ test/src/hitbox_test.cpp
@@ -12,10 +12,18 @@ class HitboxTest : public ::testing::Test {
   }
 };
 
-TEST (HitboxTest, simple_test){
+TEST (HitboxTest, simple_hitbox_overlapping_true){
     Hitbox* hitbox1 = new Hitbox(0, 0, 100, 100);
     Hitbox* hitbox2 = new Hitbox(50, 50, 100, 100);
     bool expected = true;
+    bool actual = Hitbox::is_overlapping(*hitbox1, *hitbox2);
+    EXPECT_EQ(expected, actual);   
+}
+
+TEST (HitboxTest, simple_hitbox_overlapping_false){
+    Hitbox* hitbox1 = new Hitbox(0, 0, 100, 100);
+    Hitbox* hitbox2 = new Hitbox(101, 0, 100, 100);
+    bool expected = false;
     bool actual = Hitbox::is_overlapping(*hitbox1, *hitbox2);
     EXPECT_EQ(expected, actual);   
 }

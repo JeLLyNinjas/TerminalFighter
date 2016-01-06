d391963bd44d74b3bb207122e81a66b2c4cf829c
Enoch Tsang <etsang1@hotmail.com>
2016-01-06 14:07:01 -0700

    changed is_overlapping from a static function to a member function so that it's possible to mock the function

--- src/hitbox.h
+++ src/hitbox.h
@@ -5,7 +5,8 @@ class Hitbox {
 public: 
 	Hitbox(double x_pos, double y_pos, double width, double height); 	
 	Hitbox(SDL_Rect hitbox); 
-	static bool is_overlapping(Hitbox hitbox1, Hitbox hitbox2);
+	bool is_overlapping(const Hitbox& other_hitbox);
+	const SDL_Rect& hitbox() const;
 
 protected:
 	SDL_Rect hitbox_; 
--- test/src/hitbox_test.cpp
+++ test/src/hitbox_test.cpp
@@ -9,17 +9,17 @@ protected:
 };
 
 TEST (HitboxTest, simple_hitbox_overlapping_true) {
-    Hitbox* hitbox1 = new Hitbox(0, 0, 100, 100);
-    Hitbox* hitbox2 = new Hitbox(50, 50, 100, 100);
+    Hitbox hitbox1 = Hitbox(0, 0, 100, 100);
+    Hitbox hitbox2 = Hitbox(50, 50, 100, 100);
     bool expected = true;
-    bool actual = Hitbox::is_overlapping(*hitbox1, *hitbox2);
+    bool actual = hitbox1.is_overlapping(hitbox2);
     EXPECT_EQ(expected, actual);
 }
 
 TEST (HitboxTest, simple_hitbox_overlapping_false) {
-    Hitbox* hitbox1 = new Hitbox(0, 0, 100, 100);
-    Hitbox* hitbox2 = new Hitbox(101, 0, 100, 100);
+    Hitbox hitbox1 = Hitbox(0, 0, 100, 100);
+    Hitbox hitbox2 = Hitbox(101, 0, 100, 100);
     bool expected = false;
-    bool actual = Hitbox::is_overlapping(*hitbox1, *hitbox2);
+    bool actual = hitbox1.is_overlapping(hitbox2);
     EXPECT_EQ(expected, actual);
 }

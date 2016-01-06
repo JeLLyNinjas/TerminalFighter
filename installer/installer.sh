4bfba4a5400b75edced118be3609fd7f0e596986
Enoch Tsang <etsang1@hotmail.com>
2016-01-06 02:23:30 -0700

    clenaed up installer

--- installer/installer.sh
+++ installer/installer.sh
@@ -13,15 +13,15 @@ fi
 DIR=`pwd`
 
 function finish {
-	echo "Cleaning up..."
-	rm $DIR/SDL2-2.0.3.tar.gz || true
-	rm $DIR/SDL2_ttf-2.0.12.tar.gz || true
-	rm $DIR/SDL2_image-2.0.0.tar.gz || true
-	rm $DIR/SDL2_mixer-2.0.0.tar.gz || true
-	rm -rf $DIR/SDL2_image-2.0.0  || true
-	rm -rf $DIR/SDL2_ttf-2.0.12  || true
-	rm -rf $DIR/SDL2-2.0.3 || true
-	rm -rf $DIR/SDL2_mixer-2.0.0 || true
+    echo "Cleaning up..."
+    rm $DIR/SDL2-2.0.3.tar.gz || true
+    rm $DIR/SDL2_ttf-2.0.12.tar.gz || true
+    rm $DIR/SDL2_image-2.0.0.tar.gz || true
+    rm $DIR/SDL2_mixer-2.0.0.tar.gz || true
+    rm -rf $DIR/SDL2_image-2.0.0  || true
+    rm -rf $DIR/SDL2_ttf-2.0.12  || true
+    rm -rf $DIR/SDL2-2.0.3 || true
+    rm -rf $DIR/SDL2_mixer-2.0.0 || true
 }
 
 trap finish EXIT
@@ -31,8 +31,6 @@ wget https://www.libsdl.org/release/SDL2-2.0.3.tar.gz
 tar -xzvf SDL2-2.0.3.tar.gz
 cd SDL2-2.0.3
 ./configure 
-
-
 make -j4
 make install -j4
 cd ..
@@ -56,10 +54,12 @@ cd SDL2_image-2.0.0
 make -j4
 make install -j4
 cd ..
+
 #SDL2_mixer
 wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz
 tar -xzvf SDL2_mixer-2.0.0.tar.gz
 cd SDL2_mixer-2.0.0
+
 #SMPEG
 cd external/smpeg2-2.0.0
 ./configure

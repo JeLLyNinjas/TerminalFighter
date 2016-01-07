a3a87b9d7bc856cca5e52ee51797d0ca5f2642d9
Enoch Tsang <etsang1@hotmail.com>
2016-01-07 00:19:12 -0700

    added code for correct team notify called

--- test/src/missle_launcher_test.cpp
+++ test/src/missle_launcher_test.cpp
@@ -6,6 +6,7 @@
 #include "mocks/mock_projectile_creator_listener.h"
 
 using ::testing::_;
+using ::testing::InSequence;
 
 class MissileLauncherTest : public ::testing::Test {
 protected:
@@ -31,4 +32,20 @@ TEST(MissileLauncherTest, check_listeners_notified_onlaunch) {
     MissileLauncher launcher = MissileLauncher(FRIENDLY);
     launcher.add_listener(&launcher_listener);
     launcher.create_missile(0, 0, 0, 0);
+}
+
+TEST(MissileLauncherTest, check_team_notify_called) {
+    InSequence dummy;
+
+    MockProjectileCreatorListener launcher_listener;
+    EXPECT_CALL(launcher_listener, notify_friendly_projectile_launched(_));
+    EXPECT_CALL(launcher_listener, notify_enemy_projectile_launched(_));
+
+    MissileLauncher friendly_launcher = MissileLauncher(FRIENDLY);
+    friendly_launcher.add_listener(&launcher_listener);
+    friendly_launcher.create_missile(0, 0, 0, 0);
+
+    MissileLauncher enemy_launcher = MissileLauncher(ENEMY);
+    enemy_launcher.add_listener(&launcher_listener);
+    enemy_launcher.create_missile(0, 0, 0, 0);
 }

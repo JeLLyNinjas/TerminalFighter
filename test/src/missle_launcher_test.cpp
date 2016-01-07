39e1f76abf368922b70e0b7e29fbd36adfcfc4cd
Enoch Tsang <etsang1@hotmail.com>
2016-01-06 17:23:13 -0700

    removed .Time(1) since it is alreayd implicit

--- test/src/missle_launcher_test.cpp
+++ test/src/missle_launcher_test.cpp
@@ -22,15 +22,22 @@ TEST(MissileLauncherTest, multiple_listeners_notified) {
     MockMissileLauncherListener launcher_listener2;
 
     EXPECT_CALL(launcher_listener1, notify_missile_launched(_))
-        .Times(1);
     EXPECT_CALL(launcher_listener2, notify_missile_launched(_))
-        .Times(1);
 
     launcher.add_listener(&launcher_listener2);
     launcher.create_missile(0, 0, 0, 0);
 }
 
 TEST(MissileLauncherTest, listeners_multiple_notifications) {
+    EXPECT_CALL(launcher_listener, notify_missile_launched(_))
+        .Times(3);
+
+    launcher.create_missile(0, 0, 0, 0);
+    launcher.create_missile(0, 0, 0, 0);
+    launcher.create_missile(0, 0, 0, 0);
+}
+
+TEST(MissileLauncherTest, correct_missile_passed) {
     EXPECT_CALL(launcher_listener, notify_missile_launched(_))
         .Times(3);
 

c43eea11ffcdfc8d10b2a703f1c25ec9fa39dd41
Enoch Tsang <etsang1@hotmail.com>
2016-01-07 01:02:47 -0700

    added to code to check for incrementing id of gameobjects

--- test/src/gameobject_test.cpp
+++ test/src/gameobject_test.cpp
@@ -14,8 +14,25 @@ protected:
 };
 
 TEST(GameObjectTest, check_id_start_0) {
-	GameObject* gameobject = new MockGameObject();
-	unsigned int expected = 0;
-	unsigned int actual = gameobject->id();
-	EXPECT_EQ(expected, actual);
+    GameObject* gameobject = new MockGameObject();
+    unsigned int expected = 0;
+    unsigned int actual = gameobject->id();
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(GameObjectTest, incrementing_id) {
+    const int NUM = 10;
+    GameObject* gameobjects[NUM];
+    
+    for(int i = 0; i < NUM; i++){
+        gameobjects[i] = new MockGameObject();
+    }
+
+    int prev_id = gameobjects[0]->id(); 
+
+    for(int i = 1 ; i < NUM; i++){
+        unsigned int actual = gameobjects[i]->id();
+        EXPECT_EQ(prev_id + 1, actual);
+        prev_id = gameobjects[i]->id();
+    }
 }

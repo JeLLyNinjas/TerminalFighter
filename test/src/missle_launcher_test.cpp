b1a374860bf574ff350fa8ffd7935e8361a82958
Enoch Tsang <etsang1@hotmail.com>
2016-01-06 17:20:01 -0700

    generalized some steps of missle laucnher test into the setup

--- test/src/missle_launcher_test.cpp
+++ test/src/missle_launcher_test.cpp
@@ -9,6 +9,9 @@ using ::testing::_;
 class MissleLauncherTest : public ::testing::Test {
 protected:
     virtual void SetUp() {
+        MissileLauncher launcher;
+        MockMissileLauncherListener launcher_listener;
+        launcher.add_listener(&launcher_listener);
     }
 
     virtual void TearDown() {
@@ -16,26 +19,21 @@ protected:
 };
 
 TEST(MissileLauncherTest, multiple_listeners_notified) {
-    MockMissileLauncherListener launcher_listener1;
     MockMissileLauncherListener launcher_listener2;
+
     EXPECT_CALL(launcher_listener1, notify_missile_launched(_))
         .Times(1);
     EXPECT_CALL(launcher_listener2, notify_missile_launched(_))
         .Times(1);
 
-    MissileLauncher launcher;
-    launcher.add_listener(&launcher_listener1);
     launcher.add_listener(&launcher_listener2);
     launcher.create_missile(0, 0, 0, 0);
 }
 
 TEST(MissileLauncherTest, listeners_multiple_notifications) {
-    MockMissileLauncherListener launcher_listener;
     EXPECT_CALL(launcher_listener, notify_missile_launched(_))
         .Times(3);
 
-    MissileLauncher launcher;
-    launcher.add_listener(&launcher_listener);
     launcher.create_missile(0, 0, 0, 0);
     launcher.create_missile(0, 0, 0, 0);
     launcher.create_missile(0, 0, 0, 0);

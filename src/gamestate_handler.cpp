757860af648a6b9df4db66e5a5679fcf2539a416
Enoch Tsang <etsang1@hotmail.com>
2016-01-20 21:48:21 -0700

    added a case to print a message if a game state was not found

--- src/gamestate_handler.cpp
+++ src/gamestate_handler.cpp
@@ -1,4 +1,5 @@
 #include "gamestate_handler.h"
+#include <iostream>
 
 GameStateHandler::GameStateHandler(std::map<gamestates::GameStateName, I_GameState*> &gamestates)
     : gamestates_(gamestates)
@@ -19,6 +20,10 @@ void GameStateHandler::start(gamestates::GameStateName first_state){
     for(;;){
         if(next_state == gamestates::EXIT){
             return;
+        }
+        if(gamestates_.count(next_state) == 0){
+            std::cout << "Gamestate " << next_state << " non-existent!" << std::endl;
+            exit(1);
         }
         next_state = gamestates_[next_state]->run(renderer_);
     }

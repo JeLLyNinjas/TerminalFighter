abbeb53f0ad4ec49c0faa89b7016443888088737
Byron Seto <byronseto@hotmail.com>
2017-07-10 15:49:22 -0600

    Removed futureui.xcf and changed Terminal constructor to do inline instantiation

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -10,9 +10,9 @@ SDL_Texture* Terminal::terminal_texture_ = NULL;
 
 Terminal::Terminal(double x_pos, double y_pos, double width, double height)
     : GameObject(x_pos, y_pos, 0.0, 0.0)
-    , hitbox_(Hitbox(x_pos, y_pos, width, height)) {
-    player_text_ = "";
-    default_font_ = TTF_OpenFont("/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-B.ttf", 84);
+    , hitbox_(Hitbox(x_pos, y_pos, width, height))
+    , player_text_("")
+    , default_font_ (TTF_OpenFont("/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-B.ttf", 84)) {
 }
 
 void Terminal::draw(I_GraphicsHandler& graphics) {

2884771bff3777c622011b7220e2b289e514cf23
Enoch Tsang <etsang1@hotmail.com>
2017-07-20 17:46:18 -0600

    add ignored TerminalFighter folder

---
+++ src/TerminalFighter/MainCharacter/MainCharacter.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdio.h>
+#include <memory>
+
+#include "GameObject/GameObject.h"
+#include "Hitbox/Hitbox.h"
+
+class I_Hitbox;
+class I_GraphicsHandler;
+class SDL_Texture;
+
+class MainCharacter : public GameObject {
+public:
+    MainCharacter(double x_pos, double y_pos);
+    static void set_texture(SDL_Texture* texture);
+    void add_weapon(std::unique_ptr<GameObject> weapon);
+
+    // GameObject
+    void update();
+    void draw(I_GraphicsHandler& graphics);
+    const I_Hitbox& hitbox() const;
+    void notify_collision(GameObject& collided_object);
+
+protected:
+    Hitbox hitbox_;
+
+private:
+    static SDL_Texture* texture_;
+    std::vector<std::unique_ptr<GameObject>> weapons_;
+};
---
+++ src/TerminalFighter/MenuState/MenuState.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include "../Engine/GameState/I_GameState.h"
+#include "../Engine/Delay/Delay.h"
+
+class Delay;
+class SDL_Renderer;
+
+class MenuState : public I_GameState {
+public:
+    MenuState(SDL_Renderer& renderer);
+    ~MenuState();
+    gamestates::GameStateName run();
+    gamestates::GameStateName name() const;
+
+private:
+    void display_debug_frames(Delay* delayer);
+    SDL_Renderer& renderer_;
+};

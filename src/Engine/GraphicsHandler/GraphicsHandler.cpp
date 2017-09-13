f0f2fef3d88cd2c367f809c0e13a1c02917ade23
Byron Seto <byronseto@hotmail.com>
2017-09-12 20:50:31 -0600

    Adding 3 more overloaded functions

--- src/Engine/GraphicsHandler/GraphicsHandler.cpp
+++ src/Engine/GraphicsHandler/GraphicsHandler.cpp
@@ -25,40 +25,40 @@ GraphicsHandler::GraphicsHandler(SDL_Renderer& renderer)
 }) {
 }
 
-void GraphicsHandler::draw(SDL_Texture* texture,
-                           SDL_Rect texture_rect,
-                           GraphicPriority priority,
-                           bool is_flyweight,
-                           double angle_clockwise,
-                           SDL_Point* rotation_point) {
-    draw_queue_.at(priority).push_back(DrawRequest(texture, texture_rect, is_flyweight, angle_clockwise, rotation_point));
+void GraphicsHandler::draw(SDL_Texture* texture, SDL_Rect texture_rect,
+                           GraphicPriority priority, bool cleanup, double angle_clockwise, SDL_Point* rotation_point) {
+    draw_queue_.at(priority).push_back(DrawRequest(texture, texture_rect, cleanup, angle_clockwise, rotation_point));
 }
 
-void GraphicsHandler::draw(SDL_Texture* texture,
-                           int x_pos,
-                           int y_pos,
-                           GraphicPriority priority,
-                           bool is_flyweight,
-                           double angle_clockwise,
-                           SDL_Point* rotation_point) {
+void GraphicsHandler::draw(SDL_Texture* texture, int x_pos, int y_pos,
+                           GraphicPriority priority, bool cleanup, double angle_clockwise, SDL_Point* rotation_point) {
     SDL_Rect texture_rect;
     SDL_QueryTexture(texture, NULL, NULL, &texture_rect.w, &texture_rect.h);
     texture_rect.x = x_pos;
     texture_rect.y = y_pos;
-    this->draw(texture, texture_rect, priority, is_flyweight, angle_clockwise, rotation_point);
+    this->draw(texture, texture_rect, priority, cleanup, angle_clockwise, rotation_point);
 }
 
-void GraphicsHandler::draw(SDL_Surface* surface,
-                           int x_pos,
-                           int y_pos,
-                           GraphicPriority priority,
-                           bool is_flyweight,
-                           double angle_clockwise,
-                           SDL_Point* rotation_point) {
+void GraphicsHandler::draw(SDL_Surface* surface, int x_pos, int y_pos,
+                           GraphicPriority priority, bool cleanup, double angle_clockwise, SDL_Point* rotation_point) {
     SDL_Texture* texture = SDL_CreateTextureFromSurface(&renderer_, surface);
-    this->draw(texture, x_pos, y_pos, priority, is_flyweight, angle_clockwise, rotation_point);
+    this->draw(texture, x_pos, y_pos, priority, cleanup, angle_clockwise, rotation_point);
 }
 
+void GraphicsHandler::draw(SDL_Texture* texture, SDL_Rect texture_rect,
+                           GraphicPriority priority, bool cleanup) {
+    draw(texture, texture_rect, priority, cleanup, 0, NULL);
+}
+
+void GraphicsHandler::draw(SDL_Texture* texture, int x_pos, int y_pos,
+                           GraphicPriority priority, bool cleanup) {
+    draw(texture, x_pos, y_pos, priority, cleanup, 0, NULL);
+}
+
+void GraphicsHandler::draw(SDL_Surface* surface, int x_pos, int y_pos,
+                           GraphicPriority priority, bool cleanup) {
+    draw(surface, x_pos, y_pos, priority, cleanup, 0, NULL);
+}
 
 void GraphicsHandler::update_screen() {
     for (auto priority : DRAW_ORDER) {
--- test/mocks/MockGraphicsHandler.h
+++ test/mocks/MockGraphicsHandler.h
@@ -12,6 +12,10 @@ public:
     MOCK_METHOD6(draw, void(SDL_Texture*, SDL_Rect, GraphicPriority, bool, double, SDL_Point*));
     MOCK_METHOD7(draw, void(SDL_Texture*, int, int, GraphicPriority, bool, double, SDL_Point*));
     MOCK_METHOD7(draw, void(SDL_Surface*, int, int, GraphicPriority, bool, double, SDL_Point*));
+    MOCK_METHOD4(draw, void(SDL_Texture*, SDL_Rect, GraphicPriority, bool));
+    MOCK_METHOD5(draw, void(SDL_Texture*, int, int, GraphicPriority, bool));
+    MOCK_METHOD5(draw, void(SDL_Surface*, int, int, GraphicPriority, bool));
+
     MOCK_METHOD1(load_image, SDL_Texture * (std::string));
     MOCK_METHOD0(update_screen, void());
 };

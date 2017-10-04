45161288903f2bdbae2661c20922818ba19c7d8f
Enoch Tsang <etsang1@hotmail.com>
2017-10-04 16:02:39 -0600

    add settings test

---
+++ test/SettingsTest.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "../src/TerminalFighter/Settings/Settings.h"
+
+namespace {
+    std::string TEST_VIDEO_SETTINGS_FILE = "../config.example/video_settings.yml"
+    std::string TEST_ASSET_PATHS_FILE = "../config.example/asset_paths.yml"
+}
+
+class SettingsTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+};
+
+// Mostly testing example_config is actually loadable
+TEST(SettingsTest, reload_settings) {
+    Settings settings(
+        TEST_VIDEO_SETTINGS_FILE,
+        TEST_ASSET_PATHS_FILE);
+
+    EXPECT_TRUE(reload_settings());
+}

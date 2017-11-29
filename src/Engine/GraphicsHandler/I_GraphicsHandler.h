8c7b84df356a877bee5bbc45cf24ede6c551e781
Byron Seto <byronseto@hotmail.com>
2017-11-28 23:52:14 -0700

    Adding more comments on the cleanup variable

--- src/Engine/GraphicsHandler/I_GraphicsHandler.h
+++ src/Engine/GraphicsHandler/I_GraphicsHandler.h
@@ -10,36 +10,42 @@ public:
     virtual ~I_GraphicsHandler() {}
     /**
      * Used to intiate the GraphicsHandler
-     * Currently just loads the entire vector of <code>graphic_paths</code> which are file paths
-     * to <code>game_graphics_</code>, which is a map of std::string to SDL_Textures*
+     * Loads the vector of <code>graphic_paths</code> which are file paths
+     * to <code>GraphicsHandler::game_graphics_</code>, which is a map of std::string to SDL_Textures*
      * @param path is the relative (recommended) or absolute path to graphics assets that will 
      *  be loaded when init is called. This initial load avoids having to load assets while the
      *  level has started, and can be done when "loading" the level
-     * @return has no return, but <code>internal_load_image</code> calls <code>load_image</code>
-     *  which can call <code>LOG(FATAL)</code> and exit the program when an incorrect path is called
+     * @return returns <code>void</code>. However, <code>GraphicsHandler::internal_load_image()</code> calls 
+     * <code>load_image</code> which _can_ potentially call <code>LOG(FATAL)</code>
+     * and exit the program when an incorrect path is called.
      * \note
-     * having no error handling, and immediately throwing the fail is intentional. Maybe one day later
-     * we should handle it more gracefully and have it load a dummy asset
+     * We have no real error handling here. If an invalid path is given, it means an object may not have
+     * an asset, so calling exiting the program is kind of appropriate. However, maybe one day, we should
+     * handle it gracefully and have it load a dummy asset.
      */
     virtual void init(const std::vector<std::string>& graphic_paths) = 0;
     /**
      * Draw function
      * The draw function does not immediately draw the texture. Instead, the draw function will place
      * the <code>SDL_Texture*</code> into a queue using the <code>priority</code> parameter to determine
-     * the queue order. The <code>GraphicsHandler::draw()</code> is called by the universe after the 
-     * <code>universe::update_all()</code> phase.
+     * the queue order. The queue is then drawn by <code>Universe::draw_to_screen()</code>, which is called
+     * after what should currently be the <code>Universe::draw_all()</code>. 
+     * <code>GraphicsHandler::draw()</code> is called by the universe after the 
      * @param texture will be used to SDL_RenderCopy onto the rendering target (the <code> renderer_</code>)
      * @param texture_rect is the <code>dstrect</code>, or destination rect on where the texture will
      *  be copied to. This rect defines the size, and position the texture will be rendered to
      * @param priority is used to define the order that the texture will be copied to the renderer.
      *  The GraphicPriority is an enum found in "DrawRequest/DrawRequest.h"
      * @param cleanup is used to define whether or not the draw function needs to clean up a texture.
+     *   Set this to false when the texture passed to this function is cached. 
      *   Sometimes, the texture is cached by the GraphicsHandler::game_graphics_, in which case, 
-     *   you do not want to set the bool to `true`. Other times, the texture is not cached, and
-     *   is passed into the function by an object, however, the object cannot delete the texture
-     *   after passing it to this draw function as this function does not copy the texture onto the 
-     *   renderer immediately. Refer to the main description of the draw function for explanation on 
-     *   when the SDL_RenderCopy occurs.
+     *   you do not want to set the bool to `true` or else this function will delete the cached pointer.
+     *   Set this value to true when you need the function to cleanup the texture for you.
+     *   When an object creates a texture, and will not hold onto the texture (no cache). You will need
+     *   to set the cleanup value to true. This is because the object that passes this texture onto the 
+     *   draw function is not allowed to call <code>SDL_DestroyTexture</code> to free the texture.
+     *   This function does not copy the texture onto the renderer immediately. 
+     *   Refer to the main description of the draw function for explanation on when the SDL_RenderCopy occurs.
      */
     virtual void draw(SDL_Texture* texture, SDL_Rect texture_rect, GraphicPriority priority,
                       bool cleanup, double angle_clockwise, SDL_Point* rotation_point) = 0;

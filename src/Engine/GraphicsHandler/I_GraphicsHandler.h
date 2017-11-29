79e59ffc0a091f8b9483712c96fd94af4503a3b3
Byron Seto <byronseto@hotmail.com>
2017-11-28 23:16:48 -0700

    adding more documentation to I_GraphicsHandler, and changed it to Doxygen by using /** instead of /*

--- src/Engine/GraphicsHandler/I_GraphicsHandler.h
+++ src/Engine/GraphicsHandler/I_GraphicsHandler.h
@@ -8,7 +8,7 @@
 class I_GraphicsHandler {
 public:
     virtual ~I_GraphicsHandler() {}
-    /*
+    /**
      * Used to intiate the GraphicsHandler
      * Currently just loads the entire vector of <code>graphic_paths</code> which are file paths
      * to <code>game_graphics_</code>, which is a map of std::string to SDL_Textures*
@@ -22,15 +22,24 @@ public:
      * we should handle it more gracefully and have it load a dummy asset
      */
     virtual void init(const std::vector<std::string>& graphic_paths) = 0;
-    /*
+    /**
      * Draw function
      * The draw function does not immediately draw the texture. Instead, the draw function will place
      * the <code>SDL_Texture*</code> into a queue using the <code>priority</code> parameter to determine
-     * the queue order. The textures
+     * the queue order. The <code>GraphicsHandler::draw()</code> is called by the universe after the 
+     * <code>universe::update_all()</code> phase.
      * @param texture will be used to SDL_RenderCopy onto the rendering target (the <code> renderer_</code>)
      * @param texture_rect is the <code>dstrect</code>, or destination rect on where the texture will
      *  be copied to. This rect defines the size, and position the texture will be rendered to
      * @param priority is used to define the order that the texture will be copied to the renderer.
+     *  The GraphicPriority is an enum found in "DrawRequest/DrawRequest.h"
+     * @param cleanup is used to define whether or not the draw function needs to clean up a texture.
+     *   Sometimes, the texture is cached by the GraphicsHandler::game_graphics_, in which case, 
+     *   you do not want to set the bool to `true`. Other times, the texture is not cached, and
+     *   is passed into the function by an object, however, the object cannot delete the texture
+     *   after passing it to this draw function as this function does not copy the texture onto the 
+     *   renderer immediately. Refer to the main description of the draw function for explanation on 
+     *   when the SDL_RenderCopy occurs.
      */
     virtual void draw(SDL_Texture* texture, SDL_Rect texture_rect, GraphicPriority priority,
                       bool cleanup, double angle_clockwise, SDL_Point* rotation_point) = 0;

6d4deea45965744ea5692e36c79831f6307c3d09
Tai Lai <ctlai95@gmail.com>
2017-11-30 20:12:48 -0700

    fixing the memory leak

--- src/TerminalFighter/TextArea/TextArea.cpp
+++ src/TerminalFighter/TextArea/TextArea.cpp
@@ -15,12 +15,14 @@ TextArea::~TextArea() {}
 void TextArea::render(SDL_Renderer& renderer_) {
     SDL_Surface* surface = TTF_RenderText_Solid(font_, text_str_.c_str(), color_);
     SDL_Texture* Message = SDL_CreateTextureFromSurface(&renderer_, surface);
+    SDL_FreeSurface(surface);
     SDL_Rect Message_rect;
     Message_rect.x = x_pos_;
     Message_rect.y = y_pos_;
     Message_rect.w = width_;
     Message_rect.h = height_;
     SDL_RenderCopy(&renderer_, Message, NULL, &Message_rect);
+    SDL_DestroyTexture(Message);
 }
 
 void TextArea::set_color(SDL_Color color) {

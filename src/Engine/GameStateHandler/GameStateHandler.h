788a5af123260becebcebca5868be5d35af69228
Enoch Tsang <etsang1@hotmail.com>
2017-12-02 23:21:27 -0700

    add GameStateHandler docs

--- src/Engine/GameStateHandler/GameStateHandler.h
+++ src/Engine/GameStateHandler/GameStateHandler.h
@@ -6,9 +6,23 @@
 
 #include "GameState/I_GameState.h"
 
+/**
+ * The GameStateHandler is the top level object for handling game flow.
+ * Each GameState contains internal logic on what happens in each GameState.
+ * The GameStateHandler just runs them, takes their return value and runs the next GameState.
+ */
 class GameStateHandler {
 public:
+	/**
+	 * @param gamestates is a vector of GameStates (inheriting I_GameState)
+	 * This must be a complete list of GameStates that will be run for the duration of execution.
+	 */
     GameStateHandler(std::vector<I_GameState*>& gamestates);
+
+    /**
+     * Runs all the gamestates. Conceptaully, this function should only exit when the game is exited.
+     * @param first_state defines which state is executed first.
+     */
     void start(gamestates::GameStateName first_state);
 
 private:

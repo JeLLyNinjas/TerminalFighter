88c9b2604cd2cee3bc182e16df88c345dde20446
Enoch Tsang <etsang1@hotmail.com>
2017-12-02 23:05:23 -0700

    add documentation for I_GameState

--- src/Engine/GameState/I_GameState.h
+++ src/Engine/GameState/I_GameState.h
@@ -1,5 +1,13 @@
 #pragma once
 
+/**
+ * Many objects are intended to inherit I_GameState.
+ * This is the top level type in the Terminal Fighter Engine for handling game flow.
+ * This interface allows for GameStateHandler to manage various GameStates.
+ * Each GameState individually decides what to do in each state,
+ * and what state to go to next.
+ */
+
 namespace gamestates {
     enum GameStateName {
         MAIN_MENU,
@@ -13,6 +21,16 @@ namespace gamestates {
 class I_GameState {
 public:
     virtual ~I_GameState() {}
+
+    /**
+     * Normally should run an infinite loop which draws to screen and is interactable.
+     * @return returns the next GameState to execute.
+     */
     virtual gamestates::GameStateName run() = 0;
+
+    /**
+     * @returns returns it's own GameState from the GameStateName enum
+     * The return value should be unique for every unique GameState.
+     */
     virtual gamestates::GameStateName name() const = 0;
 };

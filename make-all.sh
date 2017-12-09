398091bde6138df89157b321d4883c00e5c4a8a9
Byron Seto <byronseto@hotmail.com>
2017-12-08 20:59:33 -0700

    Removing sudo from docker command

--- make-all.sh
+++ make-all.sh
@@ -35,4 +35,6 @@ docker build . -t $IMAGE_NAME
 # "doxygen -g" will create a default config file named Doxyfile
 # "patch < Doxyfile.patch will apply the configuration patch
 
-sudo docker run --rm -v `pwd`:/documentation $IMAGE_NAME doxygen -g && patch --no-backup-if-mismatch < Doxyfile.patch && doxygen Doxyfile
+docker run --rm -v `pwd`:/documentation $IMAGE_NAME doxygen -g && patch --no-backup-if-mismatch < Doxyfile.patch && doxygen Doxyfile
+
+rm Doxyfile -f

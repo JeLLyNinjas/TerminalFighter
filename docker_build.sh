922e5de00e03a097148c9d344e1eaf72c0387c4a
Enoch Tsang <etsang1@hotmail.com>
2017-12-17 18:57:58 -0700

    new build process

--- docker_build.sh
+++ docker_build.sh
@@ -3,20 +3,20 @@
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly IMAGE_NAME="jellyninjas/terminalfighter"
 
-if [ "$1" == "init" ]; then
+if [[ $# -eq 0 ]]; then
+	echo "usage: docker_build [init | build | <build.sh args>]"
+fi
+
+if [[ "$1" == "init" ]]; then
 	docker build -t $IMAGE_NAME $DIR
-elif [ "$1" == "build" ]; then
-	docker run --rm \
-		-v $DIR:/TerminalFighter/ \
-		$IMAGE_NAME ./make-all.sh
-elif [ "$1" == "clean" ]; then
-	rm $DIR/build/CMakeCache.txt
-	rm $DIR/build/test/gtest/src/gtest-build/CMakeCache.txt
 elif [ "$1" == "shell" ]; then
 	docker run --rm -it \
 		-v $DIR:/TerminalFighter/ \
 		$IMAGE_NAME /bin/bash
 else
-	echo "usage: docker_build [init | build | shell]"
+	docker run --rm \
+		-v $DIR:/TerminalFighter/ \
+		$IMAGE_NAME ./build.sh "$1"
+	chown 1000:1000 -R .
 fi
 

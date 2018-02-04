5152c9ad0831cde0b49b247a39dbbd5582005150
Byron Seto <byronseto@hotmail.com>
2018-02-04 04:50:31 -0700

    Cleaning up errors on JN_TTF

--- src/Engine/JN_TTF/JN_TTF.cpp
+++ src/Engine/JN_TTF/JN_TTF.cpp
@@ -1,18 +1,25 @@
 #include "JN_TTF.h"
 
 JN_TTF::JN_TTF() 
+    :default_font_size_(48)
 {}
 
 JN_TTF::~JN_TTF() {
     //Delete the TTF_Font*'s within the font_map_
     for (auto it = font_map_.begin(); it != font_map_.end(); ++it) {
-        delete it->second;
+        TTF_CloseFont(it->second); //using the included TTF_CloseFont function 
     }
 }
 
 int JN_TTF::open_font(std::string path, int font_size) {
+    if (font_map_.find(path) != font_map_.end()) {
+        //Font already exists, returning 0 for no error. 
+        DLOG(INFO) << "Font " << path << " already exists";
+        return 0;
+    }
+    
     TTF_Font *temp = NULL;
-    temp = TTF_OpenFont(path, font_size);
+    temp = TTF_OpenFont(path.c_str(), font_size);
     if (temp == NULL) {
         LOG(ERROR) << "Failed to load " << path;
         return -1;
@@ -21,6 +28,16 @@ int JN_TTF::open_font(std::string path, int font_size) {
     return 0;
 }
 
-SDL_Surface *JN_TTF::TTF_RenderText(RenderType type, std::string font_type, SDL_Color color) {
-    //TTF_OpenFont(font_map_[font_type.c_str()] 
+SDL_Surface *JN_TTF::JN_TTF_RenderText(std::string text, RenderType type, std::string font_path, SDL_Color color) {
+    auto iterator = font_map_.find(font_path);
+    if (iterator == font_map_.end()) {
+        //.find could not find font_path
+        if (open_font(font_path, default_font_size_) != 0) {
+            LOG(ERROR) << "Unable to render text due to unknown font" << font_path;
+            return NULL;
+        }
+    } 
+
+    return TTF_RenderText_Blended(iterator->second, text.c_str(), color);
 }
+
--- src/Engine/JN_TTF/JN_TTF.h
+++ src/Engine/JN_TTF/JN_TTF.h
@@ -1,20 +1,24 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+#include <SDL_ttf.h>
 #include <map>
+#include <glog/logging.h>
 
 enum class RenderType {
     Blended,
     Shaded,
     Solid,
-}
+};
 
 class JN_TTF {
     public: 
         JN_TTF();
         ~JN_TTF();
         int open_font(std::string path, int font_size);
-        SDL_Surface* TTF_RenderText(RenderType type, std::string font_type, SDL_Color color);
+        SDL_Surface* JN_TTF_RenderText(std::string text, RenderType type, std::string font_path, SDL_Color color);
 
     private:
-        sdt::map<std::string, TTF_Font*> font_map_;
+        const int default_font_size_;
+        std::map<std::string, TTF_Font*> font_map_;
 };
